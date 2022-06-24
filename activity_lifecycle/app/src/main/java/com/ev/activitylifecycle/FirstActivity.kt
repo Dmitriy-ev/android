c99c5ffba5a4041d38c38e28e63ab3ff9f861034
dmitriyy.ev@gmail.com <SpD34$otf$>
2022-06-24 09:55:06 +0300

    alertDialog

--- activity_lifecycle/app/src/main/java/com/ev/activitylifecycle/FirstActivity.kt
+++ activity_lifecycle/app/src/main/java/com/ev/activitylifecycle/FirstActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ev.activitylifecycle.databinding.ActivityFirstBinding
-import com.ev.activitylifecycle.databinding.ActivitySecondBinding
 
 class FirstActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFirstBinding
--- activity_lifecycle/app/src/main/java/com/ev/activitylifecycle/SecondActivity.kt
+++ activity_lifecycle/app/src/main/java/com/ev/activitylifecycle/SecondActivity.kt
@@ -1,14 +1,27 @@
 package com.ev.activitylifecycle
 
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.ev.activitylifecycle.databinding.ActivitySecondBinding
 
-class SecondActivity: AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupAlertDialog()
+    }
+
+    private fun setupAlertDialog() {
+        val alertDialog = AlertDialog.Builder(this)
+        alertDialog.setTitle("Информационное сообщение")
+        alertDialog.setMessage("Вы во втором активити, для возврата нажмите кнопку")
+        alertDialog.setPositiveButton(
+            "К первому активити"
+        ) { _, _ -> finish() }
+        alertDialog.create()
+        alertDialog.show()
     }
 }

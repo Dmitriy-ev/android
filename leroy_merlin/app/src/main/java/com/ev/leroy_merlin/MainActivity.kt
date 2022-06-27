afb658627785009437aef88580c03f2c3240dfcf
dmitriyy.ev@gmail.com <SpD34$otf$>
2022-06-27 17:20:32 +0300

    initial_commit

---
+++ leroy_merlin/app/src/main/java/com/ev/leroy_merlin/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.ev.leroy_merlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import com.ev.leroy_merlin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        getSupportActionBar()?.hide()
+    }
+}
